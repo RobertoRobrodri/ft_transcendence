81ab04665594c807a406b7de5e8263185a8916ec
RobertoRobrodri <karmmero@gmail.com>
2024-01-07 21:52:03 +0100

    Changed name to app pong

--- back/core/core/urls.py
+++ back/core/core/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/pong_auth/', include('pong_auth.urls'))
+    path('api/pong/', include('pong.urls'))
 ]
