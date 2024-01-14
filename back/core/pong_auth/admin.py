e184ebfe6b7c327b7c0101ca427672c6d9f3a494
RobertoRobrodri <karmmero@gmail.com>
2024-01-14 20:54:56 +0100

    Added profile picture and volume

--- back/core/pong_auth/admin.py
+++ back/core/pong_auth/admin.py
@@ -6,10 +6,10 @@ from .models import CustomUser
 # Used only to manage Django admin site
 
 class CustomUserAdmin(UserAdmin):
-    list_display = ('username', 'email', 'score')  # Fields to display in the admin list
+    list_display = ('username', 'email', 'score', 'status', 'profile_picture')  # Fields to display in the admin list
     fieldsets = (
-        (None, {'fields': ('username', 'password')}),
-        ('Personal info', {'fields': ('email', 'score')}),  # Include 'score' and 'profile_picture' in admin edit view
+        (None, {'fields': ('username', 'password', 'profile_picture')}),
+        ('Personal info', {'fields': ('email', 'score', 'status')}),  # Include 'score' and 'profile_picture' in admin edit view
         ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
         ('Important dates', {'fields': ('last_login', 'date_joined')}),
     )
--- back/core/pong_auth/models.py
+++ back/core/pong_auth/models.py
@@ -2,6 +2,15 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
 class CustomUser(AbstractUser):
+	class Status(models.TextChoices):
+		OFFLINE = "offline"
+		ONLINE = "online"
+		INGAME = "ingame"
+		INQUEU = "inqueu"
+
 	score = models.IntegerField(default=0)
-#	profile_picture = models.ImageField(upload_to='profile_pictures', null=True, blank=True)
+	status = models.CharField(choices=Status, default=Status.OFFLINE)
+	friends = models.ManyToManyField('self', blank=True)
+	profile_picture = models.ImageField(upload_to='media/', null=True, blank=True)
