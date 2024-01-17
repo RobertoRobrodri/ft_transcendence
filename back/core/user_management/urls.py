5ca0fb8a1f18ab4663af132c30c26000e67c886b
RobertoRobrodri <karmmero@gmail.com>
2024-01-17 13:56:37 +0100

    Use pk instead of username

--- back/core/user_management/urls.py
+++ back/core/user_management/urls.py
@@ -1,10 +1,11 @@
 # urls.py
+# <slug:usernam>
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('user_update/<slug:username>', views.UserUpdateView.as_view(), name='user-update'),
-	path('user_delete/<slug:username>', views.UserDeleteView.as_view(), name='user-delete'),
-	path('user_list/<slug:username>', views.UserListView.as_view(), name='user-list'),
+    path('user_update/<int:pk>', views.UserUpdateView.as_view(), name='user-update'),
+	path('user_delete/<int:pk>', views.UserDeleteView.as_view(), name='user-delete'),
+	path('user_list/<int:pk>', views.UserListView.as_view(), name='user-list'),
 	path('user_list_all/', views.UserListAllView.as_view(), name='user-list-all'),
 ]
