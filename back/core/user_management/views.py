993ed4bc5105c32d909f1f8fd1be687bef558fa3
RobertoRobrodri <karmmero@gmail.com>
2024-01-22 11:38:39 +0100

    Feature Method change password for local users

--- back/core/user_management/views.py
+++ back/core/user_management/views.py
@@ -2,9 +2,10 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
 from django.shortcuts import get_object_or_404
 from pong_auth.models import CustomUser
-from .serializers import UserUpdateSerializer
+from .serializers import UserUpdateSerializer, UserUpdatePasswordSerializer
 from pong_auth.permissions import IsLoggedInUser
 
 class UserViewset(viewsets.GenericViewSet):
@@ -21,6 +22,18 @@ class UserViewset(viewsets.GenericViewSet):
 		user = get_object_or_404(self.serializer_class.Meta.model, pk=pk)
 		user_serializer = self.serializer_class(user)
 		return Response(user_serializer.data, status=status.HTTP_200_OK)
+
+	# Use this method only for local users. 42 Users will log through 42 account
+	@action(detail=True, methods=['patch'])
+	def set_password(self, request, pk=None):
+		user = get_object_or_404(self.serializer_class.Meta.model, pk=pk)
+		self.check_object_permissions(request, user)
+		if user.external_id is None:
+			user_serializer = UserUpdatePasswordSerializer(user, data=request.data)
+			if user_serializer.is_valid():
+				user_serializer.save()
+				return Response({'message': 'User updated successfully'}, status=status.HTTP_200_OK)
+		return Response({'message': 'Cannot update user'}, status=status.HTTP_400_BAD_REQUEST)
 
 	def partial_update(self, request, pk=None):
 		user = get_object_or_404(self.serializer_class.Meta.model, pk=pk)
