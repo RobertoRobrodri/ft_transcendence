cbd6a5688d05e6b2f2b31d29b51551c40980f3e5
Cristina Fernandez baldovinos <cristina_fb_26@hotmail.es>
2024-01-26 11:49:05 +0100

    login page on nginx

--- front/html/login.js
+++ front/html/login.js
@@ -1,8 +1,22 @@
-username = document.querySelector(".username")
-password = document.querySelector(".password")
+let form = document.querySelector(".logForm")
+let username = document.querySelector(".username")
+let password = document.querySelector(".password")
 
-const options = {method: 'POST', body: `{"username":${username.value},"password":${password.value}`};
-fetch('http://localhost:8000/api/pong_auth/login/', options)
-  .then(response => response.json())
+async function sendData() {
+  const options = {'mode':'no-cors', method: 'POST'};
+  // body:(
+  //   {
+  //     username : username.value,
+  //     password : password.value
+  //   })
+  
+  fetch(`http://localhost:8000/api/pong_auth/login/?username=${username.value}&password=${password.value}`, options)
   .then(response => console.log(response))
+  .then(response => console.log(response.message))
   .catch(err => console.error(err));
+}
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  sendData();
+});
