559309a01dad8c948130590e5d121724a77e99f3
RobertoRobrodri <karmmero@gmail.com>
2024-01-26 11:02:19 +0100

    Configure nginx exporter and nginx default conf allowing exporter access

--- back/core/pong_auth/models.py
+++ back/core/pong_auth/models.py
@@ -13,7 +13,7 @@ class CustomUser(AbstractUser):
 	#? Can we make them unsigned?
 	wins 			= models.IntegerField(default=0)
 	losses 			= models.IntegerField(default=0)
-	status 			= models.CharField(choices=Status, default=Status.OFFLINE)
+	status 			= models.CharField(max_length=9, choices=Status, default=Status.OFFLINE)
 	# Used for 42 Auth
 	external_id		= models.IntegerField(null=True, blank=True)
 	#TODO add default profile picture
