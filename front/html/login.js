81d4bd6ca9d1d0f64e073041be1f43234d2211dd
RobertoRobrodri <karmmero@gmail.com>
2024-02-10 15:09:31 +0100

    Login form working

--- front/html/login.js
+++ front/html/login.js
@@ -1,10 +1,44 @@
-function handleClick () {
-	alert("You are not logged in :(")
+async function handleSubmit (e) {
+	e.preventDefault()
+	// Get the input values
+	const username = document.querySelector('#username').value;
+	const password = document.querySelector('#password').value;
+
+	const loginData = {
+        username: username,
+        password: password,
+      };
+	try {
+    // Make a POST request to the specified endpoint
+    const response = await fetch('http://localhost:80/api/pong_auth/login/', {
+    	method: 'POST',
+        headers: {
+        	'Content-Type': 'application/json',
+        },body: JSON.stringify(loginData),
+    });
+    if (!response.ok) {
+    	throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    // Check if the response content type is JSON
+    const contentType = response.headers.get('content-type');
+    if (!contentType || !contentType.includes('application/json')) {
+        throw new Error('Invalid response format. Expected JSON.');
+    }
+
+    const data = await response.json();
+    // Handle the response data as needed
+    console.log(data);
+    } catch (error) {
+    	console.error('Error:', error.message);
+	}
 }
 
-export function login() {
-	const loginForm = document.querySelector('#loginButton');
-	loginForm.addEventListener('click', handleClick)
+export function login(e) {
+	// Select the login form
+	const loginForm = document.querySelector('#loginForm');
+	// Add event listener to the form submission
+	loginForm.addEventListener('submit', handleSubmit);
 }
 
 export function checkLoginStatus() {
