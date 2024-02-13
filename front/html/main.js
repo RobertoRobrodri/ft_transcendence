628d5084fc5d1560cc2b0fb30efca05343a02167
RobertoRobrodri <karmmero@gmail.com>
2024-02-13 12:26:21 +0100

    Callback is redirected

--- front/html/main.js
+++ front/html/main.js
@@ -2,10 +2,9 @@ import * as routing from "./router/router.js"
 import * as loginModule from "./login/login.js"
 import * as signUpModule from "./sing-up/signup.js"
 import * as callbackModule from "./callback/callback.js"
-import * as loaderModule from "./components/loader.js"
 
-document.addEventListener("DOMContentLoaded", loaderModule.displayLoginOrMenu);
+document.addEventListener("DOMContentLoaded", routing.router);
 loginModule.login();
 signUpModule.register();
-// callbackModule.callback42();
+callbackModule.callback42();
 window.addEventListener("hashchange", routing.router)
--- front/html/router/router.js
+++ front/html/router/router.js
@@ -1,15 +1,31 @@
 import { checkLoginStatus } from "../login/login.js"
 import { loadSignUpPage } from "../sing-up/signup.js"
-import * as callbackModule from "../callback/callback.js"
+import { callback42 } from "../callback/callback.js"
 import { displayLoginOrMenu } from "../components/loader.js"
 
 export function router() {
 	let { hash } = location;
 	console.log(hash);
-	if (checkLoginStatus() === false && !hash) {
+	// TODO if an unauthenticated user tries to see other page should be redirected to login
+	if (!hash) {
 		displayLoginOrMenu();
 	}
-	else if (hash === '#/signup') {
+	else if (hash === '#/signup' && checkLoginStatus() === false) {
 		loadSignUpPage();
 	}
+	else if (hash === '/callback' && checkLoginStatus() === false) {
+		callback42();
+	}
 }
+
+// if (checkLoginStatus === false) {
+// 	if (!hash)
+// 		loadLoginPage();
+// 	else if (hash === '#/signup') {
+// 		loadSignUpPage();
+// 	}
+// }
+// else {
+// 	if (!hash)
+// 		loadMainPage();
+// }
