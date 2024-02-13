ebc50223dec7d711f5950f567c0903e0870777a3
RobertoRobrodri <karmmero@gmail.com>
2024-02-13 19:54:39 +0100

    Clear hash after loading main page

--- front/html/menu/menu.js
+++ front/html/menu/menu.js
@@ -5,6 +5,8 @@ export function loadMainPage() {
         fetch('./menu/menu.html').then(response => response.text()),
     ]).then(([html, css]) => {
         mainPage.innerHTML = html;
+        //clear hash
+        history.pushState("", document.title, window.location.pathname + window.location.search);
     }).catch(error => {
         console.error('Error al cargar el formulario:', error);
     });
