98d72178fbd064a1e0ed09f0d8ff97a9e0e737aa
RobertoRobrodri <karmmero@gmail.com>
2024-02-14 11:50:04 +0100

    Send token to get info

--- front/html/menu/menu.js
+++ front/html/menu/menu.js
@@ -1,3 +1,13 @@
+export async function loadUserInfo() {
+    token = sessionStorage.getItem('token')
+    const response = await fetch('http://localhost:80/api/user_management/', {
+    	method: 'GET',
+        headers: {
+        	'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`,
+        }}
+    );
+}
 
 export function loadMainPage() {
 	let mainPage = document.getElementById("root");
