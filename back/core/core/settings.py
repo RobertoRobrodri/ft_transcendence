6dd6789421ebf50fcd5fb329c2c9ca6c1dcb6707
RobertoRobrodri <karmmero@gmail.com>
2024-02-25 23:41:12 +0100

    Create new Game model

--- back/core/core/settings.py
+++ back/core/core/settings.py
@@ -109,7 +109,8 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("127.0.0.1", 6379)],
+            "hosts": [('redis', 6379)],
+            "capacity": 1500,
         },
     },
 }
