35a92802b538125e612382b55d3e651d19b9a450
roberto.rodriguez <roberto.rodriguez@paack.co>
2024-03-07 13:07:26 +0100

    Include env example

--- back/core/core/settings.py
+++ back/core/core/settings.py
@@ -16,7 +16,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-DOCKERIZED = os.environ.get('DOCKERIZED')
+DOCKERIZED = os.environ.get('DOCKERIZED', "")
 
 
 # Quick-start development settings - unsuitable for production
