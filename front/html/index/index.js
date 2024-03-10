7cc0b6d3e41219dcb66d905d626be8865a8bcf67
dpavon-g <dpavon-g@student.42madrid.com>
2024-03-10 22:59:54 +0100

    Front on going

--- front/html/index/index.js
+++ front/html/index/index.js
@@ -2,8 +2,28 @@ import { loadUserInfo } from "./menu.js"
 import { connectChat } from "./chat.js"
 import { renewJWT } from "../components/updatejwt.js"
 
-export function loadMainPage() {
+function updateTime() {
+    var now = new Date();
+    var hours = now.getHours();
+    var minutes = now.getMinutes();
+    var seconds = now.getSeconds();
 
+    // Formatea la hora como HH:MM:SS
+    var formattedTime = hours + ':' + (minutes < 10 ? '0' : '') + minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
+
+    // Formatea la fecha como DD/MM/AAAA
+    var day = now.getDate();
+    var month = now.getMonth() + 1; // Se suma 1 porque los meses comienzan desde 0
+    var year = now.getFullYear();
+    var formattedDate = (day < 10 ? '0' : '') + day + '/' + (month < 10 ? '0' : '') + month + '/' + year;
+
+    // Concatena la fecha y la hora
+    var dateTimeString = formattedDate + '  |  ' + formattedTime;
+
+    // Actualiza el contenido del elemento con el ID "current-date-time"
+    document.getElementById('current-time').innerHTML = dateTimeString;
+}
+export function loadMainPage() {
     // Renew jwt
     renewJWT();
 
@@ -16,12 +36,17 @@ export function loadMainPage() {
     let mainPage = document.getElementById("root");
     Promise.all([
         fetch('./index/index.html').then(response => response.text()),
-        fetch('../styles.css').then(response => response.text())
-    ]).then(([html, css]) => {
+        fetch('../styles.css').then(response => response.text()),
+        fetch('./index/styleIndex.css').then(response => response.text())
+    ]).then(([html, css, css2]) => {
+        // Importamos el estilo base y el de esta pagina
         mainPage.innerHTML = html;
         let style = document.createElement('style');
         style.textContent = css;
         document.head.appendChild(style);
+        let style2 = document.createElement('style');
+        style2.textContent = css2;
+        document.head.appendChild(style2);
         //clear hash
         history.pushState("", document.title, window.location.pathname + window.location.search);
         loadUserInfo();
@@ -29,4 +54,10 @@ export function loadMainPage() {
     }).catch(error => {
         console.error('Error al cargar el formulario:', error);
     });
+
+    // Actualiza la hora cada segundo
+    setInterval(updateTime, 1000);
+
+    // Inicializa la hora al cargar la página
+    updateTime();
 }
