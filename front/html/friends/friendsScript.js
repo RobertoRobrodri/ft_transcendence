b39d0f7e8c52b8c8ec0f007a893c89e9a245953c
dpavon-g <pavongomezdaniel@gmail.com>
2024-04-15 21:01:04 +0200

    Diseño de panel de añadir amigos y tabla de amigos working, solo falta que llame a la api correcta para rellenar, de momento rellena con datos de ejemplo

--- front/html/friends/friendsScript.js
+++ front/html/friends/friendsScript.js
@@ -1,14 +1,16 @@
 import { renewJWT } from "../components/updatejwt.js"
 import { displayErrorList } from "../components/loader.js"
 
-export function loadFriendsPage(){
+
+
+export function loadFriendsPage() {
 
     // Remove previous styles
     const existingStyles = document.head.querySelectorAll('style');
     existingStyles.forEach(style => {
         document.head.removeChild(style);
     });
-    
+
     let loginPage = document.getElementById("root");
     Promise.all([
         fetch('./friends/friends.html').then(response => response.text()),
@@ -22,40 +24,43 @@ export function loadFriendsPage(){
     }).catch(error => {
         console.error('Error al cargar el formulario:', error);
     });
-	getFriendList();
-	FriendRequestListener();
+    loadUsersTable();
+    getFriendList();
+    FriendRequestListener();
+
 }
 
 async function getFriendList() {
-	const token = sessionStorage.getItem('token')
+    const token = sessionStorage.getItem('token')
     try {
         const response = await fetch('https://localhost:443/api/user_management/user_list/', {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
                 'Authorization': `Bearer ${token}`,
-            }}
+            }
+        }
         );
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
         const data = await response.json();
         let friend_list = document.getElementById("friend-list");
-		data.friends.forEach(friend => {
-			const listItem = document.createElement('li');
-			listItem.className = 'list-group-item';
-			listItem.textContent = friend;
-			friend_list.appendChild(listItem);
-		});
+        data.friends.forEach(friend => {
+            const listItem = document.createElement('li');
+            listItem.className = 'list-group-item';
+            listItem.textContent = friend;
+            friend_list.appendChild(listItem);
+        });
         // TODO find a way to show pending requests
-		// let pending_requests = document.getElementById("pending-requests");
-		// data.friend_requests.forEach(request => {
-		// 	const listItem = document.createElement('li');
-		// 	listItem.className = 'list-group-item';
-		// 	listItem.textContent = request;
-		// 	pending_requests.appendChild(listItem);
-		// });
-        
+        // let pending_requests = document.getElementById("pending-requests");
+        // data.friend_requests.forEach(request => {
+        // 	const listItem = document.createElement('li');
+        // 	listItem.className = 'list-group-item';
+        // 	listItem.textContent = request;
+        // 	pending_requests.appendChild(listItem);
+        // });
+
     }
     catch (error) {
         console.error('Error:', error.message);
@@ -64,29 +69,70 @@ async function getFriendList() {
     }
 }
 
+const users = [
+    { username: "user1", id: "001" },
+    { username: "user2", id: "002" },
+    { username: "user3", id: "003" },
+];
+
+async function loadUsersTable() {
+
+    const token = sessionStorage.getItem('token')
+    try {
+        const response = await fetch('https://localhost:443/api/user_management/user_list/', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`,
+            }
+        }
+        );
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const data = await response.json();
+        let userTableBody = document.getElementById("user-table-body");
+        userTableBody.innerHTML = "";
+    
+        users.forEach(user => {
+            const row = document.createElement("tr");
+            row.innerHTML = `
+                    <td>${user.username}</td>
+                    <td>${user.id}</td>
+                `;
+            userTableBody.appendChild(row);
+        });
+
+    }
+    catch (error) {
+        console.error('Error:', error.message);
+        renewJWT();
+    }
+}
+
 async function sendFriendRequest(e) {
-	const token = sessionStorage.getItem('token')
-	if (e.target.matches('#FriendRequestForm') === false)
-        return ;
+    const token = sessionStorage.getItem('token')
+    if (e.target.matches('#FriendRequestForm') === false)
+        return;
     e.preventDefault()
     // Get the input values
     const new_friend = {
-		receiver: document.querySelector('#new_friend').value,
-	}
-	try {
+        receiver: document.querySelector('#new_friend').value,
+    }
+    try {
         const response = await fetch('/api/friends/', {
             method: 'POST',
             headers: {
-            'Content-Type': 'application/json',
-			'Authorization': `Bearer ${token}`,
-            },body: JSON.stringify(new_friend),
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`,
+            }, body: JSON.stringify(new_friend),
         })
         if (!response.ok) {
             const error = await response.json();
             throw new Error(JSON.stringify(error));
         }
         const data = await response.json();
-		console.log(data);
+        console.log(data);
     }
     catch (error) {
         displayErrorList(JSON.parse(error.message), 'FriendRequestForm');
@@ -94,5 +140,5 @@ async function sendFriendRequest(e) {
 }
 
 function FriendRequestListener() {
-	document.getElementById('root').addEventListener('submit', sendFriendRequest);
+    document.getElementById('root').addEventListener('submit', sendFriendRequest);
 }
